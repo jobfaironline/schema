24e5eab9cf7dbeb48cb4d8898729c4ba225cdf41
Tien T. Truong <tientt1938@gmail.com>
2022-03-08 15:48:38 +0700

    [CAP-224] Adding job fair status

---
+++ 54_admin_job_fair_status.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS admin_job_fair_status;
+CREATE TABLE admin_job_fair_status(
+                                      jobFairId CHAR(36) PRIMARY KEY ,
+                                      status int,
+                                      FOREIGN KEY (jobFairId) REFERENCES job_fair(id)
+
+);
+
