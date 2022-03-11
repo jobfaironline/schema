e4c87e91deb57182b6604b81d1f7c26f399cd2c1
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-03-11 21:42:13 +0700

    [CAP-214] Implement draft API

--- 30_company_registration.sql
+++ 30_company_registration.sql
@@ -10,12 +10,14 @@ CREATE TABLE `company_registration`
     `cancel_reason` text,
     `admin_message` varchar(1000) DEFAULT NULL,
     `authorizer_id` char(36)      DEFAULT NULL,
-    `creator_id` char(36)       NULL,
+    `creator_id` char(36)     NOT  NULL,
     PRIMARY KEY (`id`),
     KEY `company_registration_company_id_fk` (`company_id`),
     KEY `company_registration_job_fair_id_fk` (`job_fair_id`),
+    KEY `company_registration_creator_id_fk` (`creator_id`),
     CONSTRAINT `company_registration_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`),
-    CONSTRAINT `company_registration_job_fair_id_fk` FOREIGN KEY (`job_fair_id`) REFERENCES `job_fair` (`id`)
+    CONSTRAINT `company_registration_job_fair_id_fk` FOREIGN KEY (`job_fair_id`) REFERENCES `job_fair` (`id`),
+    CONSTRAINT `company_registration_creator_id_fk` FOREIGN KEY (`creator_id`) REFERENCES `account` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
