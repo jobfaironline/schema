6220957670b25ffd4ea42f6abc7b2a14d4a7131f
Tien T. Truong <tientt1938@gmail.com>
2022-03-20 15:30:29 +0700

    add cv_activity and cv_skill

---
+++ 51_cv_skill.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `cv_skill`;
+CREATE TABLE `cv_skill`
+(
+    `id`          char(36) NOT NULL,
+    `name`        varchar(100) DEFAULT '',
+    `proficiency` int          DEFAULT '0',
+    `cv_id`       char(36)     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `cv_skill_cv_id_fk` (`cv_id`),
+    CONSTRAINT `cv_skill_cv_id_fk` FOREIGN KEY (`cv_id`) REFERENCES `cv` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
