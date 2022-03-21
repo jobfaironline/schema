2e94cf94f67afc7f7922db03928fd61b8831888e
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-03-21 09:42:37 +0700

    CAP-284 (#18)

--- 46_cv.sql
+++ 46_cv.sql
@@ -15,4 +15,4 @@ CREATE TABLE `cv` (
   CONSTRAINT `job_level_cv___fk` FOREIGN KEY (`job_level`) REFERENCES `job_level` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `cv` (`id`, `attendant_id`, `email`, `phone`, `year_of_exp`, `job_level`, `job_title`) VALUES ('d45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5','31ba4aff-a7c7-4398-9359-92fe8d305e3c','default@email.com','0912345678',0,0,'default');
+INSERT INTO `cv` (`id`, `attendant_id`, `email`, `phone`, `year_of_exp`, `job_level`, `job_title`) VALUES ('d45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5','fba88e6a-742b-4ffa-9ac7-a8058520957f','default@email.com','0912345678',0,0,'default');
