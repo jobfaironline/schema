3c1e32ffceee24da530e4a27e23678ff4d7410ba
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-07 13:57:31 +0700

    Update runfile.sh

--- runfile.sh
+++ runfile.sh
@@ -3,7 +3,7 @@ for filename in DDL/*.sql; do
         cat $filename >> schema.sql;
 done
 
-mysql -u ${{DB_USER}} -p${{DB_PASSWORD}} < schema.sql
+mysql -u $DB_USER -p$DB_PASSWORD < schema.sql
 
 rm schema.sql
 
@@ -11,6 +11,6 @@ for filename in DML/*.sql; do
         cat $filename >> schema.sql;
 done
 
-mysql -u ${{DB_USER}} -p${{DB_PASSWORD}} < schema.sql
+mysql -u $DB_USER -p$DB_PASSWORD < schema.sql
 
 rm schema.sql
