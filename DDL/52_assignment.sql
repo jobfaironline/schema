a916a6e7e150a816ca3eac19f1e2041a73035ae7
Tien T. Truong <tientt1938@gmail.com>
2022-04-07 17:36:59 +0700

    [CAP-315] - add assignment

---
+++ DDL/52_assignment.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `assignment`;
+CREATE TABLE `assignment`
+(
+    `id`                  char(36) NOT NULL,
+    `type`                int      NOT NULL,
+    `job_fair_booth_id`   char(36) DEFAULT NULL,
+    `company_employee_id` char(36) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `assignment_company_employee_account_id_fk` (`company_employee_id`),
+    KEY `assignment_job_fair_booth_id_fk` (`job_fair_booth_id`),
+    CONSTRAINT `assignment_company_employee_account_id_fk` FOREIGN KEY (`company_employee_id`) REFERENCES `company_employee` (`account_id`),
+    CONSTRAINT `assignment_job_fair_booth_id_fk` FOREIGN KEY (`job_fair_booth_id`) REFERENCES `job_fair_booth` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
