cc604a8c6c828c91a07ff40847c5f6e414b47a99
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-07 13:01:07 +0700

    Add automation script

---
+++ runfile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for filename in DDL/*.sql; do
+        cat $filename >> schema.sql;
+done
+
+mysql -u ${{DB_USER}} -p${{DB_PASSWORD}} < schema.sql
+
+rm schema.sql
+
+for filename in DML/*.sql; do
+        cat $filename >> schema.sql;
+done
+
+mysql -u ${{DB_USER}} -p${{DB_PASSWORD}} < schema.sql
+
+rm schema.sql
