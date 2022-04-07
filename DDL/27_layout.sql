b78f28626ac811080eb5ab5024cadeaa7ef0fae7
Tien T. Truong <tientt1938@gmail.com>
2022-04-07 14:36:30 +0700

    [CAP-314] - add company id for layout

--- DDL/27_layout.sql
+++ DDL/27_layout.sql
@@ -5,7 +5,10 @@ CREATE TABLE `layout`
     `name`        varchar(100)  DEFAULT NULL,
     `url`         varchar(2048) DEFAULT NULL,
     `description` varchar(500)  DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `company_id`  char(36)      DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `layout_company_id_fk` (`company_id`),
+    CONSTRAINT `layout_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
