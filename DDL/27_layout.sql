80445ad123c9e14296f0c39e7faf1341ba0dbcad
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-12 22:15:41 +0700

    Add layout thumbnail url (#25)

--- DDL/27_layout.sql
+++ DDL/27_layout.sql
@@ -6,6 +6,7 @@ CREATE TABLE `layout`
     `url`         varchar(2048) DEFAULT NULL,
     `description` varchar(500)  DEFAULT NULL,
     `company_id`  char(36)      DEFAULT NULL,
+    `thumbnail_url`   varchar(2048) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `layout_company_id_fk` (`company_id`),
     CONSTRAINT `layout_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)
