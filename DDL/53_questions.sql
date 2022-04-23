9ae8ebc0c68f8f57bd66d97790aec8e9d1641f9a
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-23 17:22:04 +0700

    Add choice and question table

--- DDL/53_questions.sql
+++ DDL/53_questions.sql
@@ -1,13 +1,13 @@
 drop table if exists questions;
 create table questions
 (
-    id            varchar(36)  not null
+    id            char(36)  not null
         primary key,
-    content       varchar(255) null,
+    content       varchar(500) null,
     createDate    bigint       null,
     updateDate    bigint       null,
     status        int          null,
-    jobPositionId varchar(36)  null,
+    jobPositionId char(36)  null,
     constraint question_job_position_id_fk
         foreign key (jobPositionId) references job_position (id)
 );
--- DDL/54_choices.sql
+++ DDL/54_choices.sql
@@ -1,10 +1,10 @@
 drop table if exists choices; 
 create table choices
 (
-    id         varchar(36)   not null,
+    id         char(36)   not null,
     content    nvarchar(255) null,
-    isCorrect  boolean       null,
-    questionId varchar(36)   null,
+    isCorrect  tinyint       null,
+    questionId char(36)      null,
     constraint choices_pk
         primary key (id),
     constraint choices_questions_id_fk
