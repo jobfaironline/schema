44bd60e6ff4209083172eeba61bed7ec6a3c9e8e
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-25 17:33:30 +0700

    Lala

--- DDL/01_3d_media.sql
+++ DDL/01_3d_media.sql
@@ -1,12 +1,11 @@
 DROP TABLE IF EXISTS `3d_media`;
 CREATE TABLE `3d_media`
 (
-    `id`          char(36)      NOT NULL,
-    `url`         varchar(2048) NOT NULL,
-    `name`        varchar(100)  NOT NULL,
-    `description` varchar(500)  NOT NULL,
-    `type`        int           NOT NULL,
+    `id`            char(36)      NOT NULL,
+    `url`           varchar(2048) NOT NULL,
+    `name`          varchar(100)  NOT NULL,
+    `description`   varchar(500)  NOT NULL,
+    `type`          int           NOT NULL,
+    `thumbnail_url` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- DML/01_3d_media.sql
+++ DML/01_3d_media.sql
@@ -1,11 +1,12 @@
-
-DELETE
-FROM `3d_media`;
-
-INSERT INTO `3d_media`
+INSERT INTO `3d_media` (`id`, `url`, `name`, `description`, `type`, 
+`thumbnail_url`)
 VALUES ('898e23ec-0049-41b4-bd02-01a8c52ff703',
-        'https://d3polnwtp0nqe6.cloudfront.net/DecorateItems/898e23ec-0049-41b4-bd02-01a8c52ff703', 'booth',
-        'this is a content to update booth', 0),
+        
+'https://d3polnwtp0nqe6.cloudfront.net/DecorateItems/898e23ec-0049-41b4-bd02-01a8c52ff703', 
+'in qui dolor',
+        'laboris sunt', 0, NULL),
        ('8b25ff4e-39a1-4af6-883e-b4253487c637',
-        'https://d3polnwtp0nqe6.cloudfront.net/DecorateItems/8b25ff4e-39a1-4af6-883e-b4253487c637', 'Chair', 'Chair 1',
-        0);
+        
+'https://d3polnwtp0nqe6.cloudfront.net/DecorateItems/8b25ff4e-39a1-4af6-883e-b4253487c637', 
+'Chair', 'Chair 1',
+        0, NULL);
