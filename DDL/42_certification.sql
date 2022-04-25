f220dc75214e4faf6f36f35a9b2baac5c0bc3e93
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-25 15:07:49 +0700

    Edit certification table (#30)
    * Edit certification table
    
    * Hello

--- DDL/42_certification.sql
+++ DDL/42_certification.sql
@@ -4,12 +4,12 @@ CREATE TABLE `certification`
     `id`                 char(36)      NOT NULL,
     `name`               varchar(1000) NOT NULL,
     `institution`        varchar(100)  DEFAULT NULL,
-    `year`               int           DEFAULT NULL,
+    `issue_date`         bigint        DEFAULT NULL,
     `certification_link` varchar(2048) DEFAULT NULL,
     `attendant_id`       char(36)      DEFAULT NULL,
+    `expired_date`       bigint        DEFAULT NULL,
+    `does_not_expired`   tinyint       DEFAULT NULL,
     PRIMARY KEY (`id`),
-    KEY `certification_attendant_id_fk` (`attendant_id`),
+    KEY                  `certification_attendant_id_fk` (`attendant_id`),
     CONSTRAINT `certification_attendant_id_fk` FOREIGN KEY (`attendant_id`) REFERENCES `attendant` (`account_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
