54366cec9bf15e12e26b7079de3f4c6b1629cc06
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-25 12:06:41 +0700

    add department into company employee (#29)

--- DDL/20_company_employee.sql
+++ DDL/20_company_employee.sql
@@ -3,6 +3,7 @@ CREATE TABLE `company_employee`
 (
     `account_id` char(36) NOT NULL,
     `company_id` char(36) DEFAULT NULL,
+    `department` varchar(100) DEFAULT NULL,
     PRIMARY KEY (`account_id`),
     KEY `company_employee_company_id_fk` (`company_id`),
     CONSTRAINT `company_employee_account_id_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
