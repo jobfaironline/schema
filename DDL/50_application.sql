a3e3ab14e040dc158b782b4c2e5cb9ffbeb24964
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-04-30 22:40:29 +0700

    Add quiz

--- DDL/50_application.sql
+++ DDL/50_application.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `application`;
 CREATE TABLE `application`
 (
-    `id`                    char(36) NOT NULL,
+    `id`                    char(36)   NOT NULL,
     `summary`               varchar(1000) DEFAULT NULL,
     `create_date`           bigint        DEFAULT NULL,
     `status`                tinyint(1) NOT NULL,
@@ -10,11 +10,17 @@ CREATE TABLE `application`
     `evaluate_date`         bigint        DEFAULT NULL,
     `authorizer_id`         char(36)      DEFAULT NULL,
     `booth_job_position_id` char(36)      DEFAULT NULL,
+    `test_status`           int           DEFAULT NULL,
     PRIMARY KEY (`id`),
-    KEY                     `cv_application_id_fk` (`cv_id`),
-    KEY                     `application_authorizer_id__fk` (`authorizer_id`),
-    KEY                     `application_booth_job_position_id_fk` (`booth_job_position_id`),
-    CONSTRAINT `application_authorizer_id__fk` FOREIGN KEY (`authorizer_id`) REFERENCES `company_employee` (`account_id`),
-    CONSTRAINT `application_booth_job_position_id_fk` FOREIGN KEY (`booth_job_position_id`) REFERENCES `booth_job_position` (`id`),
-    CONSTRAINT `cv_application_id_fk` FOREIGN KEY (`cv_id`) REFERENCES `cv` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    KEY `cv_application_id_fk` (`cv_id`),
+    KEY `application_authorizer_id__fk` (`authorizer_id`),
+    KEY `application_booth_job_position_id_fk` (`booth_job_position_id`),
+    CONSTRAINT `application_authorizer_id__fk` FOREIGN KEY 
+(`authorizer_id`) REFERENCES `company_employee` (`account_id`),
+    CONSTRAINT `application_booth_job_position_id_fk` FOREIGN KEY 
+(`booth_job_position_id`) REFERENCES `booth_job_position` (`id`),
+    CONSTRAINT `cv_application_id_fk` FOREIGN KEY (`cv_id`) REFERENCES 
+`cv` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
---
+++ DDL/55_quiz.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `quiz`;
+CREATE TABLE `quiz`
+(
+    `id`                 char(36) NOT NULL,
+    `begin_time`         bigint   DEFAULT NULL,
+    `end_time`           bigint   DEFAULT NULL,
+    `is_submitted`       tinyint  DEFAULT NULL,
+    `mark`               double   DEFAULT NULL,
+    `application_id`     char(36) DEFAULT NULL,
+    `duration`           mediumtext,
+    `number_of_question` int      DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `quiz_application_id_fk` (`application_id`),
+    CONSTRAINT `quiz_application_id_fk` FOREIGN KEY (`application_id`) 
+REFERENCES `application` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
