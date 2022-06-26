ecd445f30b3432f9df4b6cef6058a1d3a18b45ca
Tien T. Truong <53970640+TienTruong2000@users.noreply.github.com>
2022-06-27 00:26:02 +0700

    Fix bug (#47)
    * [FIX_BUG] - add assigner for assignment
    
    * add cv
    modify employee account to active status
    
    Co-authored-by: CaoSon1909 <phamcaoson1999@gmail.com>

--- DML/43_cv.sql
+++ DML/43_cv.sql
@@ -1,6 +1,6 @@
 DELETE
 FROM `cv`;
 
-INSERT INTO `cv` (`id`, `attendant_id`, `email`, `phone`, `year_of_exp`, `job_level`, `job_title`)
-VALUES ('d45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5', 'fba88e6a-742b-4ffa-9ac7-a8058520957f', 'default@email.com',
-        '0912345678', 0, 0, 'default');
+INSERT INTO `cv` (`id`, `attendant_id`, `email`, `phone`, `year_of_exp`, `job_level`, `job_title`, `name`)
+VALUES ('d45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5', '3f6eda7f-4c49-4f6c-83b2-a4c72612510a', 'default@email.com',
+        '0912345678', 0, 0, 'Nhân viên full time', 'CV số 1');
--- DML/48_cv_skill.sql
+++ DML/48_cv_skill.sql
@@ -1,2 +1,5 @@
 DELETE
 FROM `cv_skill`;
+
+INSERT INTO `cv_skill` (`id`, `name`, `proficiency`, `cv_id`) VALUES ('91e78af3-f514-44ea-a947-c86af2e137c7', 'Design', 1, 'd45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5');
+INSERT INTO `cv_skill` (`id`, `name`, `proficiency`, `cv_id`) VALUES ('c7b3c059-253c-4a57-95db-aa3825ff3e33', 'IT Supports', 2, 'd45ed5e0-a7f8-11ec-a83b-f7e48e1bdaf5');
