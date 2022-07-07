e367f4c04832b2a2d2307913b1c052606a7f9b3c
Tien T. Truong <tientt1938@gmail.com>
2022-07-07 09:04:41 +0700

    [CAP-516] - add matching point

--- DDL/31_booth_job_position.sql
+++ DDL/31_booth_job_position.sql
@@ -1,20 +1,19 @@
-
 DROP TABLE IF EXISTS `booth_job_position`;
 CREATE TABLE `booth_job_position`
 (
-    `id`                     char(36)     NOT NULL,
-    `title`                  varchar(100) NOT NULL,
-    `contact_person_name`    varchar(100) NOT NULL,
-    `contact_email`          varchar(100) NOT NULL,
-    `description`            varchar(255) NOT NULL,
-    `requirements`           varchar(500) NOT NULL,
-    `min_salary`             double       NOT NULL,
+    `id`                     char(36)      NOT NULL,
+    `title`                  varchar(100)  NOT NULL,
+    `contact_person_name`    varchar(100)  NOT NULL,
+    `contact_email`          varchar(100)  NOT NULL,
+    `description`            varchar(5000) NOT NULL,
+    `requirements`           varchar(5000) NOT NULL,
+    `min_salary`             double        NOT NULL,
     `max_salary`             double       DEFAULT NULL,
-    `num_of_position`        int          NOT NULL,
+    `num_of_position`        int           NOT NULL,
     `location_id`            char(36)     DEFAULT NULL,
-    `level_id`               int          NOT NULL,
-    `job_type_id`            int          NOT NULL,
-    `preferred_language_id`  char(3)      NOT NULL,
+    `level_id`               int           NOT NULL,
+    `job_type_id`            int           NOT NULL,
+    `preferred_language_id`  char(3)       NOT NULL,
     `origin_job_position_id` char(36)     DEFAULT NULL,
     `job_fair_booth_id`      char(36)     DEFAULT NULL,
     `is_have_test`           tinyint      DEFAULT '0',
@@ -23,12 +22,14 @@ CREATE TABLE `booth_job_position`
     `pass_mark`              int          DEFAULT '0',
     `note`                   varchar(500) DEFAULT '',
     PRIMARY KEY (`id`),
-    KEY                      `registration_job_position_job_type_id_fk` (`job_type_id`),
-    KEY                      `registration_job_position_language_id_fk` (`preferred_language_id`),
-    KEY                      `registration_job_position_level_id_fk` (`level_id`),
-    KEY                      `booth_job_position_job_fair_booth_id_fk` (`job_fair_booth_id`),
+    KEY `registration_job_position_job_type_id_fk` (`job_type_id`),
+    KEY `registration_job_position_language_id_fk` (`preferred_language_id`),
+    KEY `registration_job_position_level_id_fk` (`level_id`),
+    KEY `booth_job_position_job_fair_booth_id_fk` (`job_fair_booth_id`),
     CONSTRAINT `booth_job_position_job_fair_booth_id_fk` FOREIGN KEY (`job_fair_booth_id`) REFERENCES `job_fair_booth` (`id`),
     CONSTRAINT `registration_job_position_job_type_id_fk` FOREIGN KEY (`job_type_id`) REFERENCES `job_type` (`id`),
     CONSTRAINT `registration_job_position_language_id_fk` FOREIGN KEY (`preferred_language_id`) REFERENCES `language` (`id`),
     CONSTRAINT `registration_job_position_level_id_fk` FOREIGN KEY (`level_id`) REFERENCES `job_level` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
