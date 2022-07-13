01f1c261b631b75102f7fb8b3d0f4a04756b39ee
Tien T. Truong <tientt1938@gmail.com>
2022-07-13 09:22:29 +0700

    [CAP-532] - change default template URL

--- DML/27_layout.sql
+++ DML/27_layout.sql
@@ -3,6 +3,6 @@ FROM dbo.`layout`;
 
 INSERT INTO `layout` (`id`, `name`, `url`, `description`, `company_id`, `thumbnail_url`)
 VALUES ('b40ab83c-8f13-44ea-91b7-993f2263efae', 'template layout',
-        'https://beta1.data.jobhub.works/Layout/8eb1f379-82cb-41d6-845c-fe9c30b37f2d', 'template layout', NULL,
-        'https://d3polnwtp0nqe6.cloudfront.net/Layout-thumbnail/FuckYou.png');
+        'https://d3polnwtp0nqe6.cloudfront.net/Layout/b40ab83c-8f13-44ea-91b7-993f2263efae', 'template layout', NULL,
+        'https://d3polnwtp0nqe6.cloudfront.net/Layout-thumbnail/layout.png');
 
