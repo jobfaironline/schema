5a912f46c051bdc1818957b3a8d2ae9399ab3448
Tien T. Truong <tientt1938@gmail.com>
2022-07-27 13:58:07 +0700

    [TIEN] - add new company data

--- DML/20_company_employee.sql
+++ DML/20_company_employee.sql
@@ -2,15 +2,34 @@ DELETE
 FROM `company_employee`;
 
 INSERT INTO `company_employee`
-VALUES ('2a4b7ad3-5749-4af3-a4dc-3896d226f88f', 'MS010', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('3128aa05-fafa-4790-a8dd-219b6741f9d4', 'MS006', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+VALUES ('2a4b7ad3-5749-4af3-a4dc-3896d226f88f', 'MS007', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('2e956546-5862-483e-b12b-26bbca02f053', 'MS022', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('3128aa05-fafa-4790-a8dd-219b6741f9d4', 'MS005', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('38359950-7298-46b1-848f-8052db6ff078', 'MS023', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('413c278b-7eb5-46c8-8653-f7ee8fb6abd1', 'MS025', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('42709343-8662-4b7f-a053-1ee348b06b85', 'MS012', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
        ('4398c22f-3b23-47cb-81c2-4c6a6ddf6c73', 'MS001', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('4a8903cb-2592-4cf6-bcf1-bd97d3e49b05', 'MS011', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('5208543b-dc84-42c8-9329-74501e5babf0', 'MS012', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('79dc6dea-8a7d-4ecc-a5cf-6eeb843467f0', 'MS013', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('45ce4946-b5b3-4f5c-b26c-d134ef7ed180', 'MS015', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('4a8903cb-2592-4cf6-bcf1-bd97d3e49b05', 'MS008', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('5208543b-dc84-42c8-9329-74501e5babf0', 'MS009', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('5a0b39db-06b6-4d82-b20f-0c87fd2fdfb8', 'MS019', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('5d90179b-de15-4591-be28-12d1f613b2d8', 'MS017', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('66509cb8-0d05-4b35-9970-6c0b1e7a6434', 'MS016', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('6d54afb7-0d09-4043-8c9e-3de48ab53bd9', 'MS018', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('77b91ba7-ca9f-4c52-b3e6-54293da0ead8', 'MS013', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('79dc6dea-8a7d-4ecc-a5cf-6eeb843467f0', 'MS000', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('7dbcbd29-a26f-4388-9c6a-43bf679c14da', 'MS024', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('7eeb0db6-a76b-460a-9f79-a92085ba4518', 'MS010', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('b9367d91-c2e4-418d-8ead-4bb03f47ed9c', 'MS021', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('c252082b-c506-4420-9889-31753cbc685d', 'MS014', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
        ('c4fba70f-09d3-400b-b062-c00f96cec9e4', 'MS003', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('d52d07cc-7a7c-46af-9bdc-87a077808ae3', 'MS009', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
-       ('f996f062-c1d5-4fcc-a13c-8cae1e210d0d', 'MS005', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE');
+       ('c6a0110e-5fbb-4682-8a81-f0316e187825', 'MS002', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('d52d07cc-7a7c-46af-9bdc-87a077808ae3', 'MS006', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('e73d0985-4839-44e2-b70d-e69c347dfdbe', 'MS011', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('e7adf847-cf24-41df-964e-f0a325df8936', 'MS020', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE'),
+       ('f996f062-c1d5-4fcc-a13c-8cae1e210d0d', 'MS004', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'SE');
+
+
 
 
 
