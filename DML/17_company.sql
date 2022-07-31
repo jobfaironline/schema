056ed849870d70c06873da2b03508df0f4537a1b
Tien T. Truong <tientt1938@gmail.com>
2022-08-01 02:34:00 +0700

    [TIEN] - add company fake data

--- DML/17_company.sql
+++ DML/17_company.sql
@@ -2,8 +2,29 @@ DELETE
 FROM `company`;
 
 INSERT INTO `company`
-VALUES ('8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', '0316246614', 'Apple', 'Cupertino, California, United States',
+VALUES ('41973a70-b1d3-4495-9eae-13e9bc6a252c', '12348590a0df', 'FPT Software',
+        'Đường D1, Đ. D1, Phường Tân Phú, Quận 9, Thành phố Hồ Chí Minh 715650', '2312312313', 'fsoft@fsoft.com', 500,
+        'https://www.fpt-software.com/', 4, 0,
+        'FPT Corporation is a global leading technology and IT services provider headquartered in Vietnam, with nearly US$1.3 billion in revenue and 30,000 employees in 26 countries. As a pioneer in digital transformation, FPT delivers world-class services in Smart factory, Digital platforms, RPA, AI, IoT, Enterprise Mobility, Cloud, AR/VR, Business Applications, Application Services, BPO, and so on. The company has served over 700+ customers worldwide, a hundred of which are Fortune Global 500 companies',
+        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/41973a70-b1d3-4495-9eae-13e9bc6a252c'),
+       ('5776fa1c-468b-4476-beeb-2997a364a226', '12345678910', 'KMS',
+        '2 Tản Viên, Phường 2, Tân Bình, Thành phố Hồ Chí Minh 70000', '0930023912', 'kms@kms.com', 100,
+        'https://kms-technology.com/about-kms', 3, 0,
+        'KMS Technology, a global market leader in software development, testing services, and top-tier technology consulting offers an integrated suite of cutting-edge solutions that help businesses accelerate product speed-to-market. With a customer-centric approach, KMS Technology aims to fuel innovation for organizations by helping to modernize existing systems or bring new software visions to life.',
+        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/5776fa1c-468b-4476-beeb-2997a364a226'),
+       ('8d207faf-dbde-4d36-a79b-ef56ff308137', '123456789ab', 'Katalon',
+        '20th-21st floor, Tower B, Viettel Tower, 285 Cach Mang Thang 8 Street Ward 12, District 10 Ho Chi Minh City',
+        '03948391123', 'katalon@katalon.com', 500, 'https://katalon.com/', 3, 0,
+        'We are a global team with various backgrounds and cultures yet with the common desire to help make better software easier.\n\nWe work with the community to truly understand obstacles and roadblocks in software quality and then collaborate with others to create innovative and practical solutions.',
+        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/8d207faf-dbde-4d36-a79b-ef56ff308137'),
+       ('8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', '0316246614', 'Apple', 'Cupertino, California, United States',
         '0917499917', 'apple@apple.com', 100, 'https://www.apple.com/', 0, 0,
         'Apple Inc. is an American multinational technology company that specializes in consumer electronics, software and online services headquartered in Cupertino, California, United States.',
-        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/8e407290-9bdb-4e12-b7d3-d1ffdd1d8479');
+        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/8e407290-9bdb-4e12-b7d3-d1ffdd1d8479'),
+       ('9db38e17-cd7b-4030-bda8-4ba91282d019', '1239912312', 'Nash Tech',
+        'Etown 1, Level 3,, 364 Cong Hoa Street, Tan Binh District,, Ho Chi Minh City, Thành phố Hồ Chí Minh 736839',
+        '1231313221', 'nashtech@nashtech.com', 500, 'https://nashtechglobal.com/', 4, 0,
+        'We started with two small offices, one in London and one in Hanoi, Vietnam back in 2000 and now have over seven offices internationally, with thousands of technical experts who are constantly encouraged to challenge traditional thinking, be honest and react quickly to change.',
+        'https://d1t63ajhfi2lx8.cloudfront.net/CompanyLogo/9db38e17-cd7b-4030-bda8-4ba91282d019');
+
 
--- DML/18_company_benefit.sql
+++ DML/18_company_benefit.sql
@@ -6,3 +6,4 @@ VALUES (51, 0, '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479',
         '$200 a month kindergarten allowance per child under 5 years old'),
        (52, 1, '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479', 'Lunch and unlimited snacks');
 
+
