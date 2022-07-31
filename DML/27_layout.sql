6e3edd9deaa08a7f3218b79d7a6745c0794de616
Tien T. Truong <tientt1938@gmail.com>
2022-08-01 03:00:25 +0700

    [TIEN] - add partial fake job fair

--- DML/27_layout.sql
+++ DML/27_layout.sql
@@ -4,6 +4,11 @@ FROM dbo.`layout`;
 INSERT INTO `layout`
 VALUES ('ca8979fb-07de-448c-9da1-cd1f485a4255', 'Default layout',
         'https://d1t63ajhfi2lx8.cloudfront.net/Layout/ca8979fb-07de-448c-9da1-cd1f485a4255', 'Default layout', NULL,
-        'https://d1t63ajhfi2lx8.cloudfront.net/Layout-thumbnail/ca8979fb-07de-448c-9da1-cd1f485a4255');
+        'https://d1t63ajhfi2lx8.cloudfront.net/Layout-thumbnail/ca8979fb-07de-448c-9da1-cd1f485a4255'),
+       ('d1ba24da-bf3b-49b9-bb88-5f4136b95f12', 'My layout',
+        'https://d1t63ajhfi2lx8.cloudfront.net/Layout/d1ba24da-bf3b-49b9-bb88-5f4136b95f12', 'My layout',
+        '41973a70-b1d3-4495-9eae-13e9bc6a252c',
+        'https://d1t63ajhfi2lx8.cloudfront.net/Layout-thumbnail/d1ba24da-bf3b-49b9-bb88-5f4136b95f12');
+
 
 
--- DML/29_job_fair.sql
+++ DML/29_job_fair.sql
@@ -1,3 +1,26 @@
 DELETE
 FROM `job_fair`;
 
+INSERT INTO `job_fair`
+VALUES ('437b7546-c323-424d-87d6-9ade7b16c7c8', 1659297313466, 1659286800000, 1659459599999, 1659459600000,
+        1659632399999, 'Apple job fair event 2022 (Draft)', '123', 'Software engineer',
+        'https://d1t63ajhfi2lx8.cloudfront.net/JobFair-thumbnail/437b7546-c323-424d-87d6-9ade7b16c7c8', 0, NULL,
+        'Apple', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479'),
+       ('b2bb396f-0c96-40c1-ac17-c0ffd6b1bbb3', 1627760417000, 1627750800000, 1627923599999, 1627923600000,
+        1628096399999, 'Apple job fair 2021',
+        'Sự kiện là cầu nối giữa sinh viên và doanh nghiệp, giúp các doanh nghiệp có thể tìm được nguồn nhân lực chất lượng cao, quảng bá thương hiệu cho doanh nghiệp; Sinh viên sẽ tìm hiểu thêm về các vị trí công việc tại doanh nghiệp, năm bắt các cơ hội việc làm trong tương lai cũng như định hướng nghề nghiệp cho bản thân.',
+        'Software engineer student',
+        'https://d1t63ajhfi2lx8.cloudfront.net/JobFair-thumbnail/b2bb396f-0c96-40c1-ac17-c0ffd6b1bbb3', 1, NULL,
+        'Apple', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479'),
+       ('e9ba4bea-1b8a-4ffa-ba1e-10a5f26c3bd3', 1596225113000, 1596214800000, 1596387599999, 1596387600000,
+        1596560399999, 'Apple job fair 2020',
+        'Sự kiện là cầu nối giữa sinh viên và doanh nghiệp, giúp các doanh nghiệp có thể tìm được nguồn nhân lực chất lượng cao, quảng bá thương hiệu cho doanh nghiệp; Sinh viên sẽ tìm hiểu thêm về các vị trí công việc tại doanh nghiệp, năm bắt các cơ hội việc làm trong tương lai cũng như định hướng nghề nghiệp cho bản thân.',
+        'Software engineer student',
+        'https://d1t63ajhfi2lx8.cloudfront.net/JobFair-thumbnail/e9ba4bea-1b8a-4ffa-ba1e-10a5f26c3bd3', 1, NULL,
+        'Apple', '8e407290-9bdb-4e12-b7d3-d1ffdd1d8479'),
+       ('f927562b-36c1-4ae7-a485-d31600dbbf50', 1659296077588, 1659286800000, 1659459599999, 1659459600000,
+        1659632399999, 'Fsoft Career Day 2022',
+        'Sự kiện là cầu nối giữa sinh viên và doanh nghiệp, giúp các doanh nghiệp có thể tìm được nguồn nhân lực chất lượng cao, quảng bá thương hiệu cho doanh nghiệp; Sinh viên sẽ tìm hiểu thêm về các vị trí công việc tại doanh nghiệp, năm bắt các cơ hội việc làm trong tương lai cũng như định hướng nghề nghiệp cho bản thân.',
+        'Student from all universities',
+        'https://d1t63ajhfi2lx8.cloudfront.net/JobFair-thumbnail/f927562b-36c1-4ae7-a485-d31600dbbf50', 1, NULL,
+        'FPT Software', '41973a70-b1d3-4495-9eae-13e9bc6a252c');
