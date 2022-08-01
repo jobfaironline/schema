c75f711ed415d80cf6481eb0bf3b4925f973d270
CaoSon1909 <phamcaoson1999@gmail.com>
2022-08-01 20:56:09 +0700

    update schema account

--- DML/68_subscription_plan.sql
+++ DML/68_subscription_plan.sql
@@ -1,1 +1,5 @@
 DELETE FROM `subscription_plan`;
+
+INSERT INTO `subscription_plan` VALUES ('4b586522-eee3-4c3a-83db-95e2a4d56aa1', 'Premium package', 'Premium package', 1000);
+INSERT INTO `subscription_plan` VALUES ('5fdb64a4-a6f1-43dd-8361-1bbd8a940158', 'Basic package', 'Basic package', 600);
+INSERT INTO `subscription_plan` VALUES ('6f038fc7-4b72-4e38-b9b8-523a013da217', 'Standard package', 'Standard package', 800);
