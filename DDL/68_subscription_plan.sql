8b4bb497703af6fe392b2b47c4bbd896bb058e53
CaoSon1909 <phamcaoson1999@gmail.com>
2022-08-02 23:14:39 +0700

    update schema account

--- DDL/68_subscription_plan.sql
+++ DDL/68_subscription_plan.sql
@@ -3,10 +3,10 @@ CREATE TABLE subscription_plan
 (
     id          char(36)     not null
         primary key,
-    name        varchar(100) null,
-    description varchar(300) null,
-    price       double       null,
-    valid_period bigint      null
+    name        varchar(100) DEFAULT '',
+    description varchar(300) DEFAULT '',
+    price       double       DEFAULT '0',
+    valid_period bigint      DEFAULT '0'
 
 );
 
--- DDL/69_subscription.sql
+++ DDL/69_subscription.sql
@@ -3,15 +3,15 @@ CREATE TABLE subscription
 (
     id                     char(36)  not null
         primary key,
-    status                 int       null,
-    current_period_start   bigint    null,
-    current_period_end     bigint    null,
-    cancel_at              bigint    null,
-    default_payment_method int       null,
-    subscription_plan_id   char(36)  null,
-    company_id             char(36)  null,
-    price                  double    null,
-    transaction_id         char(100) null,
+    status                 int       DEFAULT '0',
+    current_period_start   bigint    DEFAULT '0',
+    current_period_end     bigint    DEFAULT '0',
+    cancel_at              bigint    DEFAULT '0',
+    default_payment_method int       DEFAULT '0',
+    subscription_plan_id   char(36)  NOT NULL,
+    company_id             char(36)  NOT NULL,
+    price                  double    DEFAULT '0',
+    transaction_id         char(100) DEFAULT '',
     constraint subscription_company_id_fk
         foreign key (company_id) references company (id),
     constraint subscription_subscription_plan_id_fk
