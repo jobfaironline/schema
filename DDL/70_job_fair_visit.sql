82374322778d1315c932a79e1063fbfcbd6ad7d9
CaoSon1909 <phamcaoson1999@gmail.com>
2022-08-18 21:10:29 +0700

    add notification local

---
+++ DDL/70_job_fair_visit.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `JobFairVisit`;
+
+CREATE TABLE `JobFairVisit`
+(
+    `id`             char(36) NOT NULL,
+    `jobFairId`      char(36) DEFAULT NULL,
+    `userId`         char(36) DEFAULT NULL,
+    `jobFairBoothId` char(36) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
